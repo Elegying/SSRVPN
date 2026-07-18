c6365529c467a64430e60d1808a4039d0776ae3e
Elegying <48161705+Elegying@users.noreply.github.com>
2026-07-18 11:23:40 +0800

    refactor(android): keep start rejection policy isolated

--- SSRVPN_Android/android/app/src/main/kotlin/com/ssrvpn/android/VpnServiceStartPolicy.kt
+++ SSRVPN_Android/android/app/src/main/kotlin/com/ssrvpn/android/VpnServiceStartPolicy.kt
@@ -1,5 +1,7 @@
 package com.ssrvpn.android
 
+import android.app.Service
+
 enum class RejectedServiceStartAction {
     KEEP_SERVICE,
     STOP_IDLE_SERVICE
@@ -15,5 +17,15 @@ object VpnServiceStartPolicy {
         } else {
             RejectedServiceStartAction.STOP_IDLE_SERVICE
         }
+}
 
+fun SsrvpnVpnService.finishRejectedServiceStart(
+    startId: Int,
+    action: RejectedServiceStartAction
+): Int {
+    if (action == RejectedServiceStartAction.KEEP_SERVICE) {
+        return Service.START_STICKY
+    }
+    stopSelfResult(startId)
+    return Service.START_NOT_STICKY
 }
