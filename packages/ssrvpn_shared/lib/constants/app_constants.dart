d12cc8ae843fece13a425cadce8804ccf2192a5e
Elegying <48161705+Elegying@users.noreply.github.com>
2026-08-04 15:01:57 +0800

    chore: prepare v4.0.3

--- packages/ssrvpn_shared/lib/constants/app_constants.dart
+++ packages/ssrvpn_shared/lib/constants/app_constants.dart
@@ -71,7 +71,7 @@ class AppConstants {
 
   // ── 版本信息 ──
   static const String appName = 'SSRVPN';
-  static const String appVersion = '4.0.2';
+  static const String appVersion = '4.0.3';
   static const String appUserAgent = '$appName/$appVersion';
   static const String appDescription = 'Cross-platform VPN client';
 
